a70fbbe3d3ee3a454592570d6855943f57c3643e
Roxyhuang <sixin_huang@outlook.com>
2017-09-25 19:53:14 +0800

    update .editorconfig and build.js and package.json

---
+++ build/script/build.js
@@ -0,0 +1,11 @@
+require('./check-versions')();
+
+process.env.NODE_ENV = 'production';
+
+const ora = require('ora');
+const rm = require('rimraf');
+const path = require('path');
+const chalk = require('chalk');
+const webpack = require('webpack');
+const config = require('../config');
+const webpackConfig = require('./webpack.prod.conf');
