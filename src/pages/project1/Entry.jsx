5055ff9d1e90d59e804d9bbbb108ae2f4828b0d4
roxy_huang <sixin_huang@outlook.com>
2017-10-19 10:28:05 +0800

    update lintignore

--- src/pages/project1/Entry.jsx
+++ src/pages/project1/Entry.jsx
@@ -1,2 +1,10 @@
 // Freamwork7 Example
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../assets/css/global.less';
+
+ReactDOM.render(
+  <div>123</div>,
+  document.getElementById('root')
+);
 
