d56a01803d3ffcbb37164c360000ffdff997474c
roxy_huang <sixin_huang@outlook.com>
2017-10-30 18:10:03 +0800

    del comment dnd useless code

--- build/bin/commands.js
+++ build/bin/commands.js
@@ -1,9 +1,5 @@
-const config = require('config');
 const chalk = require('chalk');
 const checkVersion = require('../script/check-versions');
-
-const host = config.get('host') || 'localhost';
-const port = config.get('port') || '3000';
 
 const option = process.argv[2];
 
@@ -14,17 +10,14 @@ switch (option) {
     console.log(chalk.green('Start lint'));
     break;
   case 'build':
-    // console.log(chalk.green('building for release...'));
     process.env.NODE_ENV = 'release';
     require('../script/build').default();
     break;
   case 'prod':
-    // console.log(chalk.green('building for prod...'));
     process.env.NODE_ENV = 'production';
     require('../script/build').default();
     break;
   case 'stage':
-    // console.log(chalk.green('building for stage...'));
     process.env.NODE_ENV = 'stage';
     require('../script/build').default();
     break;
