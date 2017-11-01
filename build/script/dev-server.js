a845bac687ea06e00d9d6824480595a5739c4ff0
roxy_huang <sixin_huang@outlook.com>
2017-11-01 17:18:48 +0800

    del content-type

--- build/script/dev-server.js
+++ build/script/dev-server.js
@@ -19,7 +19,8 @@ app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
   res.header('X-Powered-By', ' 3.2.1');
-  res.header('Content-Type', 'application/json;charset=utf-8');
+  // res.header('Content-Type', 'application/json;charset=utf-8');
+  // res.header('Content-Type', 'Content-type:application/x-javascript');
   next();
 });
 
